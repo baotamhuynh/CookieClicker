c32ec3fbc717524843c12edbd44d794b19f5f4a8
fearsomesole <makelamikael1@gmail.com>
2017-06-29 23:12:14 +0300

    Button functionality improved
    Buttons work as designed, substracting the cost from the main score and
    adding bonus counter. If there is not enough score, toast appear

--- app/src/main/java/com/example/bt/cookieclicker/MainActivity.java
+++ app/src/main/java/com/example/bt/cookieclicker/MainActivity.java
@@ -13,12 +13,12 @@ public class MainActivity extends AppCompatActivity {
 
     public int mainScore = 0;
 
-    public int normalBonusCount = 0; // bonus 1
-    public int superBonusCount = 0;  // bonus 2
-    public int duperBonusCount = 0;  // bonus 3
-    public int hyperBonusCount = 0;  // bonus 5
-    public int megaBonusCount = 0;   // bonus 8
-    public int ultraBonusCount = 0;  // bonus 13
+    public int normalBonusCount = 0;
+    public int superBonusCount = 0;
+    public int duperBonusCount = 0;
+    public int hyperBonusCount = 0;
+    public int megaBonusCount = 0;
+    public int ultraBonusCount = 0;
 
     public int normalBonusCost = 5;
     public int superBonusCost = 10;
@@ -27,18 +27,22 @@ public class MainActivity extends AppCompatActivity {
     public int megaBonusCost = 55;
     public int ultraBonusCost = 80;
 
-    public int normalBonusPerSecond;
-    public int superBonusPreSecond;
-    public int duperBonusPerSecond;
-    public int hyperBonusPerSecond;
-    public int megaBonusPerSecond;
-    public int ultraBonusPerSecond;
+    public int normalBonusPerSecond; // bonus 1
+    public int superBonusPreSecond;  // bonus 2
+    public int duperBonusPerSecond;  // bonus 3
+    public int hyperBonusPerSecond;  // bonus 5
+    public int megaBonusPerSecond;   // bonus 8
+    public int ultraBonusPerSecond;  // bonus 13
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int totalBonusScore = normalBonusPerSecond + superBonusPreSecond + duperBonusPerSecond +
+                hyperBonusPerSecond + megaBonusPerSecond + ultraBonusPerSecond;
+
     }
 
     /**
@@ -56,10 +60,14 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         normalBonusCount++;
+        mainScore -= normalBonusCost;
+        normalBonusCost += 5;
         normalBonusPerSecond = normalBonusCount;
         displayNormalBonusCalc(normalBonusPerSecond);
         TextView tvNormalCount = (TextView) findViewById(R.id.tvNormalCount);
         tvNormalCount.setText(String.valueOf(normalBonusCount));
+        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvMainScore.setText(String.valueOf(mainScore));
     }
 
     public void btSuperBonusAdd (View v){
@@ -68,10 +76,14 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         superBonusCount++;
+        mainScore -= superBonusCost;
+        superBonusCost += 10;
         superBonusPreSecond = superBonusCount * 2;
         displaySuperBonusCalc(superBonusPreSecond);
         TextView tvSuperCount = (TextView) findViewById(R.id.tvSuperCount);
         tvSuperCount.setText(String.valueOf(superBonusCount));
+        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvMainScore.setText(String.valueOf(mainScore));
     }
 
     public void btDuperBonusAdd (View v){
@@ -80,10 +92,14 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         duperBonusCount++;
+        mainScore -= duperBonusCost;
+        duperBonusCost += 15;
         duperBonusPerSecond = duperBonusCount * 3;
         displayDuperBonusCalc(duperBonusPerSecond);
         TextView tvDuperCount = (TextView) findViewById(R.id.tvDuperCount);
         tvDuperCount.setText(String.valueOf(duperBonusCount));
+        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvMainScore.setText(String.valueOf(mainScore));
     }
 
     public void btHyperBonusAdd (View v){
@@ -92,10 +108,14 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         hyperBonusCount++;
+        mainScore -= hyperBonusCost;
+        hyperBonusCost += 20;
         hyperBonusPerSecond = hyperBonusCount * 5;
         displayHyperBonusCalc(hyperBonusPerSecond);
         TextView tvHyperCount = (TextView) findViewById(R.id.tvHyperCount);
         tvHyperCount.setText(String.valueOf(hyperBonusCount));
+        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvMainScore.setText(String.valueOf(mainScore));
     }
 
     public void btMegaBonusAdd (View v){
@@ -104,10 +124,14 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         megaBonusCount++;
+        mainScore -= megaBonusCost;
+        megaBonusCost += 25;
         megaBonusPerSecond = megaBonusCount * 8;
         displayMegaBonusCalc(megaBonusPerSecond);
         TextView tvMegaCount = (TextView) findViewById(R.id.tvMegaCount);
         tvMegaCount.setText(String.valueOf(megaBonusCount));
+        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvMainScore.setText(String.valueOf(mainScore));
     }
 
     public void btUltraBonusAdd (View v){
@@ -116,10 +140,14 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         ultraBonusCount++;
+        mainScore -= ultraBonusCost;
+        ultraBonusCost += 30;
         ultraBonusPerSecond = ultraBonusCount * 13;
         displayUltraBonusCalc(ultraBonusPerSecond);
         TextView tvUltraCount = (TextView) findViewById(R.id.tvUltraCount);
         tvUltraCount.setText(String.valueOf(ultraBonusCount));
+        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvMainScore.setText(String.valueOf(mainScore));
     }
 
     /**
@@ -193,88 +221,45 @@ public class MainActivity extends AppCompatActivity {
 
 /*
     public void normalButtonOnClick() {
-        else {
-            mainScore -= normalBonusCost;
-            normalBonusCost += 5;
-            displayNormalBonusCount(normalBonusCount);
-            displayNormalBonusCalc(normalBonusCount + " * 1/s");
             mainScore = normalBonusCount * timer.scheduleAtFixedRate(task,1000,1000);
             displayScore(mainScore);
-        }
     }
     public void superButtonOnClick() {
-        else {
-            mainScore -= superBonusCost;
-            superBonusCost += 10;
-            displaySuperBonusCount(superBonusCount);
-            displaySuperBonusCalc(superBonusCount + " * 2/s");
             mainScore = superBonusCount * timer.scheduleAtFixedRate(task,1000,2000);
             displayScore(mainScore);
-        }
     }
     public void duperButtonOnClick() {
-        else {
-            mainScore -= duperBonusCost;
-            duperBonusCost += 15;
-            displayDuperBonusCount(duperBonusCount);
-            displayDuperBonusCalc(duperBonusCount + " * 3/s");
             mainScore = duperBonusCount * timer.scheduleAtFixedRate(task,1000,3000);
             displayScore(mainScore);
-        }
     }
     public void hyperButtonOnClick() {
-        else {
-            mainScore -= hyperBonusCost;
-            hyperBonusCost += 20;
-            hyperBonusCount++;
-            displayHyperBonusCount(HyperBonusCount);
-            displayHyperBonusCalc(HyperBonusCount + " * 4/s");
             mainScore = hyperBonusCount * timer.scheduleAtFixedRate(task,1000,4000);
             displayScore(mainScore);
-        }
     }
     public void megaButtonOnClick() {
-        else {
-            mainScore -= megaBonusCost;
-            megaBonusCost += 25;
-            megaBonusCount++;
-            displayMegaBonusCount(megaBonusCount);
-            displayMegaBonusCalc(megaBonusCount + " * 5/s");
             mainScore = megaBonusCount * timer.scheduleAtFixedRate(task,1000,5000);
             displayScore(mainScore);
-        }
     }
     public void ultraButtonOnClick() {
-        else {
-            mainScore -= ultraBonusCost;
-            ultraBonusCost += 30;
-            ultraBonusCount++;
-            displayUltraBonusCount(ultraBonusCount);
-            displayDuperBonusCalc(ultraBonusCount + " * 6/s");
             mainScore = ultraBonusCount * timer.scheduleAtFixedRate(task,1000,6000);
             displayScore(mainScore);
-        }
-    }
-
-
-    public void scorePerSec() {
-    }
-
-    public void resetButtonOnClick() {
-        int mainScore = 0;
-
-        int normalBonusCount = 0;
-        int superBonusCount = 0;
-        int duperBonusCount = 0;
-        int hyperBonusCount = 0;
-        int megaBonusCount = 0;
-        int ultraBonusCount = 0;
-
-        int normalBonusCost = 5;
-        int superBonusCost = 10;
-        int duperBonusCost = 20;
-        int hyperBonusCost = 35;
-        int megaBonusCost = 55;
-        int ultraBonusCost = 80;
     }*/
+
+    public void ResetButtonOnClick() {
+        mainScore = 0;
+
+        normalBonusCount = 0;
+        superBonusCount = 0;
+        duperBonusCount = 0;
+        hyperBonusCount = 0;
+        megaBonusCount = 0;
+        ultraBonusCount = 0;
+
+        normalBonusCost = 5;
+        superBonusCost = 10;
+        duperBonusCost = 20;
+        hyperBonusCost = 35;
+        megaBonusCost = 55;
+        ultraBonusCost = 80;
+    }
 }
