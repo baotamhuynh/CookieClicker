405f73160ecc4b026bf7d6285fbfb53e6e29e08d
fearsomesole <makelamikael1@gmail.com>
2017-07-02 00:45:20 +0300

    improvements on the user experience
    added overflow menu, made buttons disable when no points, changed colors

--- app/src/main/java/com/example/bt/cookieclicker/MainActivity.java
+++ app/src/main/java/com/example/bt/cookieclicker/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.bt.cookieclicker;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
     public int mainScore = 0;
 
     public TextView tvMainScore;
+    public TextView tvNormalCount;
+    public TextView tvNormalCost;
+    public TextView tvSuperCount;
+    public TextView tvSuperCost;
+    public TextView tvDuperCount;
+    public TextView tvDuperCost;
+    public TextView tvHyperCount;
+    public TextView tvHyperCost;
+    public TextView tvMegaCount;
+    public TextView tvMegaCost;
+    public TextView tvUltraCount;
+    public TextView tvUltraCost;
+
+    public Button btNormalBonus;
+    public Button btSuperBonus;
+    public Button btDuperBonus;
+    public Button btHyperBonus;
+    public Button btMegaBonus;
+    public Button btUltraBonus;
 
     public Timer timer;
 
@@ -47,6 +71,13 @@ public class MainActivity extends AppCompatActivity {
         timer = new Timer();
         tvMainScore = (TextView) findViewById(R.id.tvMainScore);
 
+        btNormalBonus = (Button) findViewById(R.id.btNormalBonus);
+        btSuperBonus = (Button) findViewById(R.id.btSuperBonus);
+        btDuperBonus = (Button) findViewById(R.id.btDuperBonus);
+        btHyperBonus = (Button) findViewById(R.id.btHyperBonus);
+        btMegaBonus = (Button) findViewById(R.id.btMegaBonus);
+        btUltraBonus = (Button) findViewById(R.id.btUltraBonus);
+
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -55,173 +86,183 @@ public class MainActivity extends AppCompatActivity {
                         mainScore += normalBonusPerSecond + superBonusPreSecond + duperBonusPerSecond +
                                 hyperBonusPerSecond + megaBonusPerSecond + ultraBonusPerSecond;
                         tvMainScore.setText(String.valueOf(mainScore));
+
                     }
                 });
             }
         }, 1000, 1000);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        CheckIfEnoughPoints();
+                    }
+                });
+            }
+        }, 100, 100);
     }
 
-    /**
-     *
-     */
     public void ClickMeButtonOnClick(View v) {
         mainScore++;
-        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
         tvMainScore.setText(String.valueOf(mainScore));
     }
 
-    public void btNormalBonusAdd (View v){
-        if (mainScore < normalBonusCost) {
-            Toast.makeText(this, "You do not have enough points.", Toast.LENGTH_SHORT).show();
-            return;
+    public void CheckIfEnoughPoints() {
+
+        if (mainScore >= normalBonusCost) {
+            btNormalBonus.setEnabled(true);
         }
+
+        else{btNormalBonus.setEnabled(false);}
+
+        if (mainScore >= superBonusCost) {
+            btSuperBonus.setEnabled(true);
+        }
+
+        else{btSuperBonus.setEnabled(false);}
+
+        if (mainScore >= duperBonusCost) {
+            btDuperBonus.setEnabled(true);
+        }
+
+        else{btDuperBonus.setEnabled(false);}
+
+        if (mainScore >= hyperBonusCost) {
+            btHyperBonus.setEnabled(true);
+        }
+
+        else{btHyperBonus.setEnabled(false);}
+
+        if (mainScore >= megaBonusCost) {
+            btMegaBonus.setEnabled(true);
+        }
+
+        else{btMegaBonus.setEnabled(false);}
+
+        if (mainScore >= ultraBonusCost) {
+            btUltraBonus.setEnabled(true);
+        }
+
+        else{btUltraBonus.setEnabled(false);}
+    }
+
+    public void NormalBonusAdd (View v){
+        tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvNormalCount = (TextView) findViewById(R.id.tvNormalCount);
+        tvNormalCost = (TextView) findViewById(R.id.tvNormalCost);
+
         normalBonusCount++;
         mainScore -= normalBonusCost;
         normalBonusCost += 5;
         normalBonusPerSecond = normalBonusCount;
         displayNormalBonusCalc(normalBonusPerSecond);
-        TextView tvNormalCount = (TextView) findViewById(R.id.tvNormalCount);
         tvNormalCount.setText(String.valueOf(normalBonusCount));
-        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
         tvMainScore.setText(String.valueOf(mainScore));
+        tvNormalCost.setText(String.valueOf("Cost: " + normalBonusCost));
     }
 
-    public void btSuperBonusAdd (View v){
-        if (mainScore < superBonusCost) {
-            Toast.makeText(this, "You do not have enough points.", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    public void SuperBonusAdd (View v){
+        tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvSuperCount = (TextView) findViewById(R.id.tvSuperCount);
+        tvSuperCost = (TextView) findViewById(R.id.tvSuperCost);
+
         superBonusCount++;
         mainScore -= superBonusCost;
         superBonusCost += 10;
         superBonusPreSecond = superBonusCount * 2;
         displaySuperBonusCalc(superBonusPreSecond);
-        TextView tvSuperCount = (TextView) findViewById(R.id.tvSuperCount);
         tvSuperCount.setText(String.valueOf(superBonusCount));
-        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
         tvMainScore.setText(String.valueOf(mainScore));
+        tvSuperCost.setText(String.valueOf("Cost: " + superBonusCost));
 
     }
 
-    public void btDuperBonusAdd (View v){
-        if (mainScore < duperBonusCost) {
-            Toast.makeText(this, "You do not have enough points.", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    public void DuperBonusAdd (View v){
+        tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvDuperCount = (TextView) findViewById(R.id.tvDuperCount);
+        tvDuperCost = (TextView) findViewById(R.id.tvDuperCost);
+
         duperBonusCount++;
         mainScore -= duperBonusCost;
-        duperBonusCost += 15;
+        duperBonusCost += 20;
         duperBonusPerSecond = duperBonusCount * 3;
         displayDuperBonusCalc(duperBonusPerSecond);
-        TextView tvDuperCount = (TextView) findViewById(R.id.tvDuperCount);
         tvDuperCount.setText(String.valueOf(duperBonusCount));
-        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
         tvMainScore.setText(String.valueOf(mainScore));
+        tvDuperCost.setText(String.valueOf("Cost: " + duperBonusCost));
     }
 
-    public void btHyperBonusAdd (View v){
-        if (mainScore < hyperBonusCost) {
-            Toast.makeText(this, "You do not have enough points.", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    public void HyperBonusAdd (View v){
+        tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvHyperCount = (TextView) findViewById(R.id.tvHyperCount);
+        tvHyperCost = (TextView) findViewById(R.id.tvHyperCost);
+
         hyperBonusCount++;
         mainScore -= hyperBonusCost;
-        hyperBonusCost += 20;
+        hyperBonusCost += 35;
         hyperBonusPerSecond = hyperBonusCount * 5;
         displayHyperBonusCalc(hyperBonusPerSecond);
-        TextView tvHyperCount = (TextView) findViewById(R.id.tvHyperCount);
         tvHyperCount.setText(String.valueOf(hyperBonusCount));
-        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
         tvMainScore.setText(String.valueOf(mainScore));
+        tvHyperCost.setText(String.valueOf("Cost: " + hyperBonusCost));
     }
 
-    public void btMegaBonusAdd (View v){
-        if (mainScore < megaBonusCost) {
-            Toast.makeText(this, "You do not have enough points.", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    public void MegaBonusAdd (View v){
+        tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvMegaCount = (TextView) findViewById(R.id.tvMegaCount);
+        tvMegaCost = (TextView) findViewById(R.id.tvMegaCost);
+
         megaBonusCount++;
         mainScore -= megaBonusCost;
-        megaBonusCost += 25;
+        megaBonusCost += 55;
         megaBonusPerSecond = megaBonusCount * 8;
         displayMegaBonusCalc(megaBonusPerSecond);
-        TextView tvMegaCount = (TextView) findViewById(R.id.tvMegaCount);
         tvMegaCount.setText(String.valueOf(megaBonusCount));
-        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
         tvMainScore.setText(String.valueOf(mainScore));
+        tvMegaCost.setText(String.valueOf("Cost: " + megaBonusCost));
     }
 
-    public void btUltraBonusAdd (View v){
-        if (mainScore < ultraBonusCost) {
-            Toast.makeText(this, "You do not have enough points.", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    public void UltraBonusAdd (View v){
+        tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvUltraCount = (TextView) findViewById(R.id.tvUltraCount);
+        tvUltraCost = (TextView) findViewById(R.id.tvUltraCost);
+
         ultraBonusCount++;
         mainScore -= ultraBonusCost;
-        ultraBonusCost += 30;
+        ultraBonusCost += 80;
         ultraBonusPerSecond = ultraBonusCount * 13;
         displayUltraBonusCalc(ultraBonusPerSecond);
-        TextView tvUltraCount = (TextView) findViewById(R.id.tvUltraCount);
         tvUltraCount.setText(String.valueOf(ultraBonusCount));
-        TextView tvMainScore = (TextView) findViewById(R.id.tvMainScore);
         tvMainScore.setText(String.valueOf(mainScore));
+        tvUltraCost.setText(String.valueOf("Cost: " + ultraBonusCost));
     }
 
-    /**
-     * Displays the calculation of normal over time bonus
-     *
-     * @param score
-     */
     public void displayNormalBonusCalc(int score) {
         TextView scoreSet = (TextView) findViewById(R.id.tvNormalCalc);
         scoreSet.setText(String.valueOf(score + "/s"));
     }
 
-    /**
-     * Displays the calculation of super over time bonus
-     *
-     * @param score
-     */
     public void displaySuperBonusCalc(int score) {
         TextView scoreSet = (TextView) findViewById(R.id.tvSuperCalc);
         scoreSet.setText(String.valueOf(score + "/s"));
     }
 
-    /**
-     * Displays the calculation of duper over time bonus
-     *
-     * @param score
-     */
     public void displayDuperBonusCalc(int score) {
         TextView scoreSet = (TextView) findViewById(R.id.tvDuperCalc);
         scoreSet.setText(String.valueOf(score + "/s"));
     }
 
-    /**
-     * Displays the calculation of hyper over time bonus
-     *
-     * @param score
-     */
     public void displayHyperBonusCalc(int score) {
         TextView scoreSet = (TextView) findViewById(R.id.tvHyperCalc);
         scoreSet.setText(String.valueOf(score + "/s"));
     }
 
-    /**
-     * Displays the calculation of mega over time bonus
-     *
-     * @param score
-     */
     public void displayMegaBonusCalc(int score) {
         TextView scoreSet = (TextView) findViewById(R.id.tvMegaCalc);
         scoreSet.setText(String.valueOf(score + "/s"));
     }
 
-    /**
-     * Displays the calculation of ultra over time bonus
-     *
-     * @param score
-     */
     public void displayUltraBonusCalc(int score) {
         TextView scoreSet = (TextView) findViewById(R.id.tvUltraCalc);
         scoreSet.setText(String.valueOf(score + "/s"));
@@ -229,20 +270,90 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void ResetButtonOnClick() {
+        tvMainScore = (TextView) findViewById(R.id.tvMainScore);
+        tvNormalCount = (TextView) findViewById(R.id.tvNormalCount);
+        tvNormalCost = (TextView) findViewById(R.id.tvNormalCost);
+        tvSuperCount = (TextView) findViewById(R.id.tvSuperCount);
+        tvSuperCost = (TextView) findViewById(R.id.tvSuperCost);
+        tvDuperCount = (TextView) findViewById(R.id.tvDuperCount);
+        tvDuperCost = (TextView) findViewById(R.id.tvDuperCost);
+        tvHyperCount = (TextView) findViewById(R.id.tvHyperCount);
+        tvHyperCost = (TextView) findViewById(R.id.tvHyperCost);
+        tvMegaCount = (TextView) findViewById(R.id.tvMegaCount);
+        tvMegaCost = (TextView) findViewById(R.id.tvMegaCost);
+        tvUltraCount = (TextView) findViewById(R.id.tvUltraCount);
+        tvUltraCost = (TextView) findViewById(R.id.tvUltraCost);
+
         mainScore = 0;
+        tvMainScore.setText(String.valueOf(0));
 
         normalBonusCount = 0;
+        tvNormalCount.setText(String.valueOf(0));
         superBonusCount = 0;
+        tvSuperCount.setText(String.valueOf(0));
         duperBonusCount = 0;
+        tvDuperCount.setText(String.valueOf(0));
         hyperBonusCount = 0;
+        tvHyperCount.setText(String.valueOf(0));
         megaBonusCount = 0;
+        tvMegaCount.setText(String.valueOf(0));
         ultraBonusCount = 0;
+        tvUltraCount.setText(String.valueOf(0));
 
         normalBonusCost = 5;
+        tvNormalCost.setText(String.valueOf("Cost: " + 5));
         superBonusCost = 10;
+        tvSuperCost.setText(String.valueOf("Cost: " + 10));
         duperBonusCost = 20;
+        tvDuperCost.setText(String.valueOf("Cost: " + 20));
         hyperBonusCost = 35;
+        tvHyperCost.setText(String.valueOf("Cost: " + 35));
         megaBonusCost = 55;
+        tvMegaCost.setText(String.valueOf("Cost: " + 55));
         ultraBonusCost = 80;
+        tvUltraCost.setText(String.valueOf("Cost: " + 80));
+
+        normalBonusPerSecond = 0;
+        superBonusPreSecond = 0;
+        duperBonusPerSecond = 0;
+        hyperBonusPerSecond = 0;
+        megaBonusPerSecond = 0;
+        ultraBonusPerSecond = 0;
+
+        displayNormalBonusCalc(0);
+        displaySuperBonusCalc(0);
+        displayDuperBonusCalc(0);
+        displayHyperBonusCalc(0);
+        displayMegaBonusCalc(0);
+        displayUltraBonusCalc(0);
+
+        Toast.makeText(this, "You just reset the game!", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.overflow_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_reset: {
+                ResetButtonOnClick();
+                break;
+            }
+            case R.id.action_about: {
+                AlertDialog.Builder about = new AlertDialog.Builder(this, R.style.Theme_AppCompat_Dialog);
+                about.setTitle("About us!")
+                .setMessage("This app was created by Mikael Mäkelä and Bao Tam Huynh for the Android App Development -summer course.")
+                .setNeutralButton("Coolio!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                }).show();
+                break;
+            }
+        }
+        return false;
     }
 }
